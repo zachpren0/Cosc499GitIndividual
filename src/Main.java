b179abcc30be46b314237d668889039afec4796e
zachpren0 <77454945+zachpren0@users.noreply.github.com>
2022-09-20 18:29:34 -0700

    Main Class Added
    Main Java class added , this class contains a static void class that will generate two arrays of random numbers and random characters.

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// this class will create an array of integers of size 5
+		Random random = new Random();
+		int[] intArr = new int [5];
+		char[]charArr = new char [5];
+		for (int i = 0; i < intArr.length; i++) {
+			
+			int temp = random.nextInt(10);
+			
+			intArr[i]= temp;
+			
+					System.out.print(intArr[i]);	
+			}
+		
+		System.out.println();
+		
+		for (int i = 0; i < charArr.length; i++) {
+		
+		char tempchar = (char)(random.nextInt(26) + 'a');
+		
+		charArr[i] = tempchar;
+		System.out.print(charArr[i]);
+		}
+		
+		
+
+	}
+
+}
+
